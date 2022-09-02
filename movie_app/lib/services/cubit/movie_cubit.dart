d4c9e39c8ec07160e50747c1f3dd499de51b01d0
SANTOSH NAGARGOJE <nsantosh435@gmail.com>
2022-09-02 18:46:48 +0530

    Final Back Up

--- movie_app/lib/services/cubit/movie_cubit.dart
+++ movie_app/lib/services/cubit/movie_cubit.dart
@@ -2,23 +2,29 @@ import 'dart:developer';
 import 'package:bloc/bloc.dart';
 import 'package:movie_app/services/cubit/movie_state.dart';
 import 'package:movie_app/services/rest_api/api_services.dart';
+import 'package:movie_app/utils/constants.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class MoviesCubit extends Cubit<MoviesState> {
   MoviesCubit() : super(InitialState()) {
     displayData();
   }
 
-  ApiServices repository = ApiServices();
-
   displayData() async {
-    bool? isLoaded;
+    bool isLoaded = false;
     emit(InitialState());
-    // ignore: unnecessary_null_comparison
-    if (isLoaded != false) {
+    ApiServices repository = ApiServices();
+    final SharedPreferences sharedPreferences =
+        await SharedPreferences.getInstance();
+    bool? isLoggedIn = sharedPreferences.getBool(isLoggedKey);
+
+    if (isLoggedIn = true) {
+      log("$isLoggedIn");
       isLoaded = true;
       final movieList = await repository.getMovie();
       log("inside MoviesCubit $movieList");
       emit(LoadedState(movieList));
+      log("isLoggedIn $isLoggedIn");
     } else {
       isLoaded = false;
       log("Else MoviesCubit $isLoaded");
