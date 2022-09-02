1ae594432daed94260dd8509fc21735668efe584
SANTOSH NAGARGOJE <nsantosh435@gmail.com>
2022-09-02 10:44:49 +0530

    Sign in and home screen display issue

--- movie_app/lib/main.dart
+++ movie_app/lib/main.dart
@@ -1,32 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:movie_app/screen/home_screen.dart';
 import 'package:movie_app/screen/signin_screen.dart';
-import 'package:movie_app/services/cubit/movie_cubit.dart';
+import 'package:movie_app/services/auth/auth_services.dart';
+import 'package:movie_app/services/auth_bloc/auth_bloc.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
+   Authentication? authentication;
   runApp(
     MaterialApp(
-      home: BlocProvider(
-        create: ((context) => MoviesCubit()),
-        child: const HomeScreen(),
+      home: BlocProvider<AuthBloc>(
+        create: (context) => AuthBloc(authentication!),
+        child: const Signin(),
       ),
       debugShowCheckedModeBanner: false,
     ),
-//     MultiBlocProvider(
-//   providers: [
-//     BlocProvider<BlocA>(
-//       create: (BuildContext context) => BlocA(),
-//     ),
-//     BlocProvider<BlocB>(
-//       create: (BuildContext context) => BlocB(),
-//     ),
-//     BlocProvider<BlocC>(
-//       create: (BuildContext context) => BlocC(),
-//     ),
-//   ],
-//   child: ChildA(),
-// )
   );
 }
--- movie_app/lib/services/rest_api/api_services.dart
+++ movie_app/lib/services/rest_api/api_services.dart
@@ -25,3 +25,31 @@ class ApiServices {
     return sample;
   }
 }
+
+// import 'dart:convert';
+// import 'dart:developer';
+// import 'package:movie_app/model/movie.dart';
+// import 'package:http/http.dart' as http;
+
+// class ApiServices {
+//   final List<Movie> sample = [];
+//   Future<List<Movie>> getMovie() async {
+//     var client = http.Client();
+
+//     var uri = Uri.parse("https://jsonplaceholder.typicode.com/photos");
+//     var response = await client.get(uri);
+
+//     if (response.statusCode == 200) {
+//       try {
+//         var json = response.body;
+//         //   log("in side ApiServices  $json");
+//         final sample = movieFromJson(json);
+//         log("in side ApiServices  $sample");
+//         return sample;
+//       } catch (e) {
+//         print(e.toString());
+//       }
+//     }
+//     return sample;
+//   }
+// }
