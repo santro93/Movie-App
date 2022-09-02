d03a6f96f6289991412b2c6bc1c5ed704a2b3a11
SANTOSH NAGARGOJE <nsantosh435@gmail.com>
2022-09-02 11:17:54 +0530

    MultiProvider

--- movie_app/lib/main.dart
+++ movie_app/lib/main.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movie_app/screen/home_screen.dart';
 import 'package:movie_app/screen/signin_screen.dart';
 import 'package:movie_app/services/auth/auth_services.dart';
 import 'package:movie_app/services/auth_bloc/auth_bloc.dart';
+import 'package:movie_app/services/cubit/movie_cubit.dart';
 
 void main() {
-   Authentication? authentication;
   runApp(
     MaterialApp(
-      home: BlocProvider<AuthBloc>(
-        create: (context) => AuthBloc(authentication!),
+      home: MultiBlocProvider(
+        providers: [
+          BlocProvider<AuthBloc>(
+            create: (context) => AuthBloc(Authentication()),
+            child: const HomeScreen(),
+          ),
+          BlocProvider<MoviesCubit>(
+            create: (context) => MoviesCubit(),
+            child: const HomeScreen(),
+          ),
+        ],
         child: const Signin(),
       ),
       debugShowCheckedModeBanner: false,
